ee90579ef77a53fa76d1e56970a0151a7d74e1e7
Gustavo Sales Oliveira <gsalesol@everis.com>
2021-07-28 09:37:15 -0300

    Primeiro Commit

---
+++ src/test/java/br/com/alura/leilao/acceptence/steps/PropondoLanceSteps.java
@@ -0,0 +1,136 @@
+package br.com.alura.leilao.acceptence.steps;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+//import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+
+import br.com.alura.leilao.model.Lance;
+import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Usuario;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
+
+public class PropondoLanceSteps {
+
+	private Lance lance;
+	private Leilao leilao;
+	//private Lance lance10;
+	//private Lance lance15;
+	private ArrayList<Lance> lista;
+	
+	@Before
+	public void setup() {
+		this.lista = new ArrayList<Lance>();
+		leilao = new Leilao("Tablet XPTO");
+		//System.out.println("before");
+	}
+	
+	@After
+	public void tearDown() {
+		//System.out.println("after");
+	}
+	
+	@Dado("un lance valido")
+	public void dado_un_lance_valido() {
+		
+		Usuario usuario = new Usuario("fulano");
+		lance = new Lance(usuario, BigDecimal.TEN);
+		//leilao = new Leilao("Tablet XPTO");
+	}
+
+
+	@Quando("propoe ao leilao")
+	public void quando_propoe_o_leilao() {
+		
+		leilao.propoe(lance);
+	    
+	}
+	@Entao("o lance eh aceito")
+	public void entao_o_lance_eh_aceito() {
+	    
+		Assert.assertEquals(1, leilao.getLances().size());
+		Assert.assertEquals(BigDecimal.TEN, leilao.getLances().get(0).getValor());
+		
+	}
+	
+//	@Dado("varios lances validos")
+//	public void varios_lances_validos() {
+//
+//		Usuario usuario1 = new Usuario("fulano");
+//		lance10 = new Lance(usuario1, BigDecimal.TEN);
+//		Usuario usuario2 = new Usuario("beltrano");
+//		lance15 = new Lance(usuario2, new BigDecimal("15.0"));
+//		leilao = new Leilao("Tablet XPTO");
+//	}
+	
+	
+	@Dado("um lance de {double} reais do usuario {string}")
+	public void um_lance_de_reais_do_usuario_fulano(Double valor,String nomeUsuario) {
+	    Lance lance = new Lance(new Usuario(nomeUsuario), new BigDecimal(valor));
+		//System.out.println(valor);
+	    //System.out.println(nomeUsuario);
+	    lista.add(lance);
+	}
+
+
+
+	@Quando("propoe varios lances leilao")
+	public void propoe_varios_lances_leilao() {
+		
+		this.lista.forEach(lance -> leilao.propoe(lance));
+		//leilao.propoe(lance10);
+		//leilao.propoe(lance15);
+		
+
+	}
+	@Entao("os lance sao aceitos")
+	public void os_lance_sao_aceitos() {
+		
+		Assert.assertEquals(this.lista.size(), leilao.getLances().size());
+		Assert.assertEquals(this.lista.get(0).getValor(), leilao.getLances().get(0).getValor());
+		Assert.assertEquals(this.lista.get(1).getValor(), leilao.getLances().get(1).getValor());
+	}
+	
+	@Dado("um lance invalido de {double} reais e do usuario {string}")
+	public void um_lance_invalido_de_reais(Double valor, String nomeUsuario) {
+		//System.out.println(nomeUsuario);
+		this.lance = new Lance(new BigDecimal(valor));
+	}
+	
+	@Entao("o lance nao eh aceito")
+	public void o_lance_nao_eh_aceito() {
+		
+		Assert.assertEquals(0, leilao.getLances().size());
+	}
+	
+	@Dado("dois lances")
+	public void dois_lances(DataTable dataTable) {
+	  
+	    List<Map<String,String>> valores = dataTable.asMaps();
+	    for (Map<String,String> mapa : valores) {
+	    	//System.out.println(mapa.keySet());
+	    	String valor = mapa.get("valor");
+	    	String nome = mapa.get("nomeUsuario");
+	    	Lance lance = new Lance(new Usuario(nome), new BigDecimal(valor));
+	    	lista.add(lance);
+			
+		}
+	}
+
+	
+	@Entao("o segundo lance nao eh aceito")
+	public void o_segundo_lance_nao_eh_aceito() {
+		Assert.assertEquals(1, leilao.getLances().size());
+		Assert.assertEquals(this.lista.get(0).getValor(), leilao.getLances().get(0).getValor());
+	}
+
+}
